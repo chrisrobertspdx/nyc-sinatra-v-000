98b6a89a2e183fdea0e7887232f1e7055d097b07
Chris Roberts <chrisrobertspdx@gmail.com>
2017-11-25 23:02:56 +0000

    Done.

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -1,3 +1,30 @@
 class LandmarksController < ApplicationController
+  get "/landmarks" do
 
+    erb :index
+  end
+
+  get "/landmarks/new" do
+    erb :'landmarks/new'
+  end
+
+  post "/landmarks" do
+
+    redirect to "/landmarks/#{@figure.id}"
+  end
+
+  get "/landmarks/:id" do
+
+    erb :show
+  end
+
+  get "/landmarks/:id/edit" do
+
+    erb :edit
+  end
+
+  post "/landmarks/:id" do
+
+    redirect to "/figures/#{@figure.id}"
+  end
 end
